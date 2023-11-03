bbf1fab5255c9aa5ad3b11596ec60e06fd2edbdd
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-11-03 03:17:37 +0200

    add mongoDB

---
+++ database.py
@@ -0,0 +1,26 @@
+import pymongo
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize the MongoDB client
+client = pymongo.MongoClient(os.getenv("CONNECTION_STRING"))
+
+# Select the database for your bot
+db = client["functions_db"]
+
+ideas_collection = db["ideas"]
+
+# Store an idea in the collection
+def store_idea(ai_question: str):
+    idea = {
+        "function_idea": ai_question,
+    }
+
+    ideas_collection.insert_one(idea)
+
+
+def get_ideas():
+    return ideas_collection.find()
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
 from commands import set_commands
+from database import store_idea, get_ideas
 
 
 load_dotenv()
@@ -47,15 +48,16 @@ async def ask_question(message: types.Message):
     ai_developer_answer = generate_ai_developer_answer(ai_question)
     ai_tester_answer = generate_ai_tester_answer(ai_developer_answer)
 
-    await message.answer(f'I generate ideas. {ai_question}')
-    await message.reply(f'I write code. {ai_developer_answer}')
-    await message.reply(f'I write tests. {ai_tester_answer}')
+    await message.answer(f'{hbold("Idea Generator")}. {ai_question}')
+    await message.reply(f'{hbold("Developer")}. {ai_developer_answer}')
+    await message.reply(f'{hbold("Tester")}. {ai_tester_answer}')
 
 
 @dp.message()
 async def chat_gpt(message: types.Message) -> None:
+    existing_ideas = [idea["function_idea"] for idea in get_ideas()]
     # Define a system message to set the behavior or context of the assistant.
-    system_message = {"role": "system", "content": "You are a helpful assistant."}
+    system_message = {"role": "system", "content": f"You are a helpful assistant, that checks what functions are in the DB ({', '.join(existing_ideas)}) and if user question is about one of them answers user. But if user's question not about one of DB functions you appologies that can't provide information about something that not in the DB."}
 
     # Create a user message using the content of the received message.
     user_message = {"role": "user", "content": message.text}
@@ -76,18 +78,36 @@ async def chat_gpt(message: types.Message) -> None:
 
 
 def generate_ai_question():
+    # Retrieve all existing ideas from the database
+    existing_ideas = [idea["function_idea"] for idea in get_ideas()]
+
+    # Create a message instructing OpenAI to analyze previous answers
+    user_message = {
+        "role": "user",
+        "content": f"Analyze all your previous answers, that are in the DB ({', '.join(existing_ideas)}), and generate an idea of the function that is not in DB yet. Don't write code of that function."
+    }
+
     # Use OpenAI to generate an AI question
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful creator of ideas."},
-            {"role": "user", "content": "Suggest to write any function from any standard Python libraries. Don't give examples of code."},
+            user_message,
         ],
         temperature=0.8,
         max_tokens=128,
     )
-    
-    return response.choices[0]['message']['content']
+
+    ai_answer = response.choices[0]['message']['content']
+
+    # Check if the generated question already exists in the database
+    if ai_answer not in existing_ideas:
+        store_idea(ai_answer)
+        return ai_answer
+
+    # If the generated question already exists in the database, try again
+    return generate_ai_question()
+
 
 def generate_ai_developer_answer(ai_question):
     # Use OpenAI to generate an AI answer to the AI-generated question
